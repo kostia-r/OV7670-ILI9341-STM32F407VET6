2a1a1b59071995487e4f2ff36e3e8ac197860c2f
kostia-r <90792188+kostia-r@users.noreply.github.com>
2024-12-01 13:53:30 +0100

    Application: reconfigured ISR priorities - the issue has been solved. (#3)

--- Core/Src/gpio.c
+++ Core/Src/gpio.c
@@ -101,10 +101,10 @@ void MX_GPIO_Init(void)
   HAL_GPIO_Init(GPIOD, &GPIO_InitStruct);
 
   /* EXTI interrupt init*/
-  HAL_NVIC_SetPriority(EXTI0_IRQn, 1, 0);
+  HAL_NVIC_SetPriority(EXTI0_IRQn, 0, 0);
   HAL_NVIC_EnableIRQ(EXTI0_IRQn);
 
-  HAL_NVIC_SetPriority(EXTI1_IRQn, 1, 0);
+  HAL_NVIC_SetPriority(EXTI1_IRQn, 0, 0);
   HAL_NVIC_EnableIRQ(EXTI1_IRQn);
 
   HAL_NVIC_SetPriority(EXTI3_IRQn, 0, 0);
